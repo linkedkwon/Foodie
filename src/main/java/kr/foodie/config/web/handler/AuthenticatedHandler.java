a563b76dec169385acb9089c05450ff2dee482cd
linkedkwon <linkedkwon@gmail.com>
2021-04-16 14:24:27 +0900

    Modify main header and authenticated dto

--- src/main/java/kr/foodie/config/web/handler/AuthenticatedHandler.java
+++ src/main/java/kr/foodie/config/web/handler/AuthenticatedHandler.java
@@ -8,13 +8,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
 
-/**
- * only for detail page as footer template
- */
+
 @Component
-public class DetailAuthenticatedHandler implements HandlerInterceptor {
+public class AuthenticatedHandler implements HandlerInterceptor {
 
     private static final String principalValue = "anonymousUser";
 
@@ -23,24 +20,15 @@ public class DetailAuthenticatedHandler implements HandlerInterceptor {
                            Object handler, ModelAndView modelAndView) throws Exception {
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-
         if(auth.getPrincipal().toString().equalsIgnoreCase(principalValue)) {
-            modelAndView.addObject("map", getMap(false));
+            modelAndView.addObject("authenticated", "0");
+            modelAndView.addObject("iconRed","/images/ico-login-red.png");
+            modelAndView.addObject("iconNormal","/images/ico-login.png");
+
             return;
         }
-        modelAndView.addObject("map", getMap(true));
-    }
-
-    private static Map<String, String> getMap(boolean authenticated){
-        if(authenticated){
-            return Map.of(
-                    "footer-li-name-1", "로그아웃", "footer-li-name-2","마이페이지",
-                    "footer-li-url-1","/logout", "footer-li-url-2","/user/info"
-            );
-        }
-        return Map.of(
-                "footer-li-name-1","로그인", "footer-li-name-2","회원가입",
-                "footer-li-url-1","/auth/login", "footer-li-url-2","/auth/join/user1"
-        );
+        modelAndView.addObject("authenticated","1");
+        modelAndView.addObject("iconRed","/images/1223.png");
+        modelAndView.addObject("iconNormal","/images/1223.png");
     }
 }
