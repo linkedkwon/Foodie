7586ce849723c89ebba9287547ac1d82a749da68
linkedkwon <linkedkwon@gmail.com>
2021-08-10 20:55:19 +0900

    Modify inquiry-form, css size for IE

--- src/main/resources/static/js/inquiry.js
+++ src/main/resources/static/js/inquiry.js
@@ -4,23 +4,16 @@ function checkInquiryFormBeforeSubmit(){
     var content = $('#input-content').val();
     var flag = true;
 
-    if(title.length < 4 || title.length > 30){
-        $('#input-title').css('color','#FF0000')
-        $('#input-title').val('제목은 4~100자 내외로 작성해야 합니다.');
-        flag = false;
-    }else if(title == '제목을 입력해주세요.' || title == '제목을 다시 확인해주세요.' || title == '제목은 4~100자 내외로 작성해야 합니다.'){
-        $('#input-title').css('color','#FF0000')
-        $('#input-title').val('제목을 다시 확인해주세요.');
+    if(title.length < 4 || title.length > 30 || title == '제목을 다시 확인해주세요.(4~100자 내외)' || title == '제목을 입력해주세요.'){
+        $('#input-title').val('제목을 다시 확인해주세요.(4~100자 내외)');
+        $('#input-title').css('color','#FF0000');
         flag = false;
     }
 
-    if(content.length < 10 || content.length > 1000){
-        $('#input-content').css('color','#FF0000')
-        $('#input-content').val('내용은 4~1000자 내외로 작성해야 합니다.');
+    if(content.length < 4 || content.length > 1000 || content == '내용을 다시 확인해주세요.(4~1000자 내외)' || content == '문의할 내용을 입력해주세요.'){
+        $('#input-content').val('내용을 다시 확인해주세요.(4~1000자 내외)');
+        $('#input-content').css('color','#FF0000');
         flag = false;
-    }else if(content == '문의할 내용을 입력해주세요.' || content == '내용을 다시 확인해주세요.' || content == '내용은 4~1000자 내외로 작성해야 합니다.'){
-        $('#input-content').css('color','#FF0000')
-        $('#input-content').val('내용을 다시 확인해주세요.');
     }
 
     if(flag == false) return;
@@ -69,16 +62,17 @@ function hideDeleteAllPopUp(){ document.getElementById("all-delete-modal").style
 
 function oninputTitle(){
     var title = $('#input-title').val();
-    if(title == '제목을 입력해주세요.' || title == '제목을 다시 확인해주세요.' || '제목은 4~100자 내외로 작성해야 합니다.') {
+    if(title == '제목을 다시 확인해주세요.(4~100자 내외)') {
+        console.log('씨팔2');
         $('#input-title').val('');
         $('#input-title').css('color', '#898989');
     }
 }
+
 function oninputContent(){
     var content = $('#input-content').val();
-    if(content == '문의할 내용을 입력해주세요.' || content == '내용을 다시 확인해주세요.' || '내용은 4~1000자 내외로 작성해야 합니다.') {
+    if(content == '내용을 다시 확인해주세요.(4~1000자 내외)') {
         $('#input-content').val('');
         $('#input-content').css('color', '#898989');
     }
 }
-
