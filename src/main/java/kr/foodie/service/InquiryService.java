296b1d8f2ebea6cc0f1e487ebd1ec5ebc92cc3a5
linkedkwon <linkedkwon@gmail.com>
2021-08-14 09:54:41 +0900

    Modify submain, signup, mypage css, js for IE

--- src/main/java/kr/foodie/service/InquiryService.java
+++ src/main/java/kr/foodie/service/InquiryService.java
@@ -9,6 +9,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.transaction.Transactional;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
 
