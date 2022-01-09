03f4b4016ad82b4c52619684a566ece3d20a7617
linkedkwon <linkedkwon@gmail.com>
2022-01-10 00:01:44 +0900

    Add admin review list

--- src/main/java/kr/foodie/controller/AdminReviewController.java
+++ src/main/java/kr/foodie/controller/AdminReviewController.java
@@ -7,31 +7,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
 @Controller
-@RequestMapping("/admin/review")
+@RequestMapping("/admin")
 public class AdminReviewController {
 
     private final AdminReviewService adminReviewService;
 
-    @GetMapping("/all")
+    @GetMapping("/review/all")
     public String renderAdminReviewList(){
         return "admin-comment-list";
     }
 
     @ResponseBody
-    @PostMapping("/Qnldd")
+    @PostMapping("/review/list")
     public Map<String, Object> getReviewInfo(@ModelAttribute AdminReviewListVO vo){
         Map<String, Object> data = new HashMap<>();
         data.put("currentPage", vo.getPage() % 10 == 0 ? 9 : vo.getPage() % 10 - 1);
         data.put("item", adminReviewService.getAdminReviewList(vo));
         int size = adminReviewService.getAllReviewCount(vo);
         data.put("size", size);
-        //data.put("pages", adminReviewService.getPages(vo, size));
-        //data.put("btns", adminReviewService.getBtnPages(vo, size));
+        data.put("pages", adminReviewService.getPages(vo, size));
+        data.put("btns", adminReviewService.getBtnPages(vo, size));
 
         return data;
+    }
+
+    @ResponseBody
+    @PostMapping("/delete/review")
+    public String deleteCheckedList(@RequestParam(value ="list[]") List<Integer> list){
+        return adminReviewService.deleteUserById(list);
     }
 }
--- src/main/java/kr/foodie/service/admin/AdminReviewService.java
+++ src/main/java/kr/foodie/service/admin/AdminReviewService.java
@@ -2,6 +2,9 @@ package kr.foodie.service.admin;
 
 import kr.foodie.domain.account.AdminReviewListVO;
 import kr.foodie.domain.account.Review;
+import kr.foodie.domain.account.ReviewDTO;
+import kr.foodie.domain.user.AdminUserListVO;
+import kr.foodie.repo.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +12,11 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
@@ -18,9 +25,19 @@ public class AdminReviewService {
     @PersistenceContext
     private EntityManager em;
 
-    public List<Review> getAdminReviewList(AdminReviewListVO vo){
-        String jpql = "select r from Review r ";
-        TypedQuery<Review> query = em.createQuery(jpql, Review.class)
+    private final ReviewRepository reviewRepository;
+
+    private static final String join = "from Review r left join User u "
+                            +"on r.userId = u.id "
+                            +"left join Shop s "
+                            +"on s.shopId = r.shopId";
+
+    public List<ReviewDTO> getAdminReviewList(AdminReviewListVO vo){
+        String jpql = "select new kr.foodie.domain.account.ReviewDTO("
+                +"r.shopId, s.shopName,r.userId,  u.name,  r.reviewId, r.starRating, r.content, r.bestComment, "
+                +"function('date_format', r.createdTime, '%Y-%m-%d')) " + join;
+
+        TypedQuery<ReviewDTO> query = em.createQuery(addQueryByFilter(vo, jpql), ReviewDTO.class)
                 .setFirstResult((vo.getPage() - 1) * 15)
                 .setMaxResults(15);
 
@@ -28,25 +45,89 @@ public class AdminReviewService {
     }
 
     public int getAllReviewCount(AdminReviewListVO vo){
-        String jpql = "select count(r) from Review r ";
+        String jpql = "select count(r) " + join;
         Query query = em.createQuery(addQueryByFilter(vo, jpql));
 
         return Math.toIntExact((long) query.getSingleResult());
     }
 
-    private String addQueryByFilter(AdminReviewListVO vo, String jpql){
-        if(!vo.getKeyword().equals("-1")) {
-            if (vo.getOption().equals("이름"))
-                jpql += "and r.email like '%" + vo.getKeyword() + "%'";
-            else if(vo.getOption().equals("제목"))
-                jpql += "and s.name like '%" + vo.getKeyword() + "%'";
-            else if(vo.getOption().equals("내용"))
-                jpql += "and s.name like '%" + vo.getKeyword() + "%'";
+    public List<Integer> getPages(AdminReviewListVO vo, int size){
+
+        List<Integer> pages = new ArrayList<>();
+        int pageSize = size / 15;
+        pageSize += size % 15 > 0 ? 1 : 0;
+
+        int left = 1;
+        if(vo.getPage() == 1) {
+            left = 1;
+        }
+        else{
+            int currentPage = vo.getPage();
+            left = currentPage % 10 == 0 ?
+                    (currentPage / 10 - 1) * 10 + 1 : (currentPage / 10) * 10 + 1;
         }
 
-        jpql += "order by s.createdDate desc";
+        for(int i = left; i <= pageSize; i++){
+            if(i == left + 10)
+                break;
+            pages.add(i);
+        }
+        return pages;
+    }
+
+    public Map<String, Integer> getBtnPages(AdminReviewListVO vo, int size){
+        int pageSize = size / 15;
+        pageSize += size % 15 > 0 ? 1 : 0;
+
+        int page = vo.getPage();
+
+        int nextFlag = page % 10 == 0 ? (page / 10 - 1) * 10 + 11 : (page / 10) * 10 + 11;
+        int prevFlag = page % 10 == 0 ? (page / 10 - 2) * 10 + 1: (page / 10 - 1) * 10 + 1;
+
+        Map<String, Integer> btnValues = new HashMap<>();
+        btnValues.put("first", page - 11 >= 0 ? 1 : -1);
+        btnValues.put("prev", page - 11 >= 0 ? prevFlag : -1);
+        btnValues.put("last", nextFlag <= pageSize ? pageSize : -1);
+        btnValues.put("next", nextFlag <= pageSize ? nextFlag : -1);
+
+        return btnValues;
+    }
+
+    private String addQueryByFilter(AdminReviewListVO vo, String jpql){
+        boolean hasWhereCaluse = false;
+
+        if(!vo.getKeyword().equals("-1")) {
+            if (vo.getOption().equals("이름")) {
+                jpql += " where u.name like '%" + vo.getKeyword() + "%'";
+                hasWhereCaluse = true;
+            }
+            else if(vo.getOption().equals("가게명")) {
+                jpql += " where s.shopName like '%" + vo.getKeyword() + "%'";
+                hasWhereCaluse = true;
+            }
+            else if(vo.getOption().equals("내용")) {
+                jpql += " where r.content like '%" + vo.getKeyword() + "%'";
+                hasWhereCaluse = true;
+            }
+        }
+
+        if(!vo.getBestOption().equals("-1")) {
+            if(!hasWhereCaluse)
+                jpql += " where r.bestComment like '%" + vo.getBestOption() + "%'";
+            else
+                jpql += " and r.bestComment like '%" + vo.getBestOption() + "%'";
+        }
+
+        jpql += " order by r.createdTime desc";
 
         return jpql;
     }
 
+    @Transactional
+    public String deleteUserById(List<Integer> list){
+        for(int o : list){
+            reviewRepository.deleteByReviewId(o);
+        }
+        return "1";
+    }
 }
