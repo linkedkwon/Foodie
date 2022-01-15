8f953c4350f0bbb12b6a852d5dc07654e911e758
LiiNen <kjeonghoon065@gmail.com>
2022-01-15 16:19:14 +0900

    Add modifying info patch api

--- src/main/java/kr/foodie/controller/UserController.java
+++ src/main/java/kr/foodie/controller/UserController.java
@@ -1,9 +1,12 @@
 package kr.foodie.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import kr.foodie.VO.UserVO;
 import kr.foodie.entity.UserEntity;
 import kr.foodie.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,5 +18,11 @@ public class UserController {
   @GetMapping("/userId/{id}")
   public UserEntity findByUserId(@PathVariable int id) {
     return userRepository.findByUserId(id);
+  }
+
+  @PatchMapping("")
+  public String patchUser(@RequestBody UserVO userVO) {
+    userRepository.updateUser(userVO.getName(), userVO.getPoint(), userVO.getUserId());
+    return "success";
   }
 }
--- src/main/java/kr/foodie/repository/UserRepository.java
+++ src/main/java/kr/foodie/repository/UserRepository.java
@@ -2,9 +2,22 @@ package kr.foodie.repository;
 
 import kr.foodie.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
   UserEntity findByUserId(int userId);
+
+  @Transactional
+  @Modifying
+  @Query(value="update user set name=:name, point=:point where user_id=:userId", nativeQuery = true)
+  void updateUser(
+          @Param("name") String name,
+          @Param("point") int point,
+          @Param("userId") int userId
+  );
 }
